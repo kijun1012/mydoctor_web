bd1f260cadebf6c6fe651eddef6fe29b64953dd6
kijun <kijun1012@nate.com>
2017-04-03 19:12:48 +0900

    init first my doctor

---
+++ src/main/java/com/mydoctor/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.mydoctor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+
+		return "home";
+	}
+
+}
