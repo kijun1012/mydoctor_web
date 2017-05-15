5f2b97318356d47258b21624000e5fbcfb2a7bd7
kijun <kijun1012@nate.com>
2017-05-16 00:10:04 +0900

    로그인 성공 핸들러 (role에 따른 구분)

---
+++ src/main/java/com/mydoctor/config/UrlAuthenticationSuccessHandler.java
@@ -0,0 +1,60 @@
+package com.mydoctor.config;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+public class UrlAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException {
+		handle(request, response, authentication);
+		clearAuthenticationAttributes(request);
+	}
+
+	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException {
+		String targetUrl = determineTargetUrl(authentication);
+
+		if (response.isCommitted()) {
+			logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+			return;
+		}
+
+		redirectStrategy.sendRedirect(request, response, targetUrl);
+	}
+
+	protected String determineTargetUrl(Authentication authentication) {
+
+		String role = authentication.getAuthorities().toString();
+		System.out.println(role);
+		String targetUrl = "";
+
+		if (role.contains("ROLE_USER")) {
+			targetUrl = "/";
+			System.out.println("유저당");
+		} else if (role.contains("ROLE_ADMIN")) {
+			targetUrl = "/doctor";
+			System.out.println("의사당");
+		}
+		return targetUrl;
+	}
+
+	public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+		this.redirectStrategy = redirectStrategy;
+	}
+
+	protected RedirectStrategy getRedirectStrategy() {
+		return redirectStrategy;
+	}
+
+}
