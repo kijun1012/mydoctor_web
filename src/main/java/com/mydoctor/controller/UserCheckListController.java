1fc29b6fbe7dffd561f65327d4b25865bb48ce4b
GyuminDev <pleasure082@gmail.com>
2017-05-18 00:36:14 +0900

    CheckList/set 요청 시 Weight을 Weight테이블의 저장 기능 추가

--- src/main/java/com/mydoctor/controller/UserCheckListController.java
+++ src/main/java/com/mydoctor/controller/UserCheckListController.java
@@ -21,33 +21,23 @@ import com.mydoctor.service.WeightService;
 @Controller
 @RequestMapping("/checklist")
 public class UserCheckListController {
-	
+
 	@Autowired
 	private UserCheckListService userCheckListService;
-	
+
 	@Autowired
 	private WeightService weightService;
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/set", method = RequestMethod.POST, produces = "application/json")
 	public ResponseEntity<UserCheckList> udpateCheckList(@RequestBody UserCheckList checkList) {
-		
-		
+
 		System.out.println(checkList);
-		
+
 		UserCheckList currentUserCheckList = userCheckListService.findById(checkList.getUsername());
-		if (currentUserCheckList == null) {
-			throw new UserNotFoundException(checkList.getUsername());
-		}
-		
-		currentUserCheckList.setAge(checkList.getAge());
-		currentUserCheckList.setHeight(checkList.getHeight());
-		currentUserCheckList.setIsalcohol(checkList.getIsalcohol());
-		currentUserCheckList.setIssmoke(checkList.getIssmoke());
-		currentUserCheckList.setWeight(checkList.getWeight());
-		currentUserCheckList.setSex(checkList.getSex());
-		
-		//weight 테이블에 레코드 없는 경우 checklist/set 요청 올때 weight을 테이블에 추가
+
+		// checklist/set 요청 올때 weight을 테이블에 추가
+
 		Weight weight = new Weight();
 		weight.setUsername(checkList.getUsername());
 		Date date = new Date();
@@ -55,14 +45,24 @@ public class UserCheckListController {
 		String newdate = format1.format(date);
 		weight.setMeasurement_time(newdate);
 		weight.setWeightValue(checkList.getWeight());
-		
+
+		if (currentUserCheckList == null) {
+			throw new UserNotFoundException(checkList.getUsername());
+		}
+
+		currentUserCheckList.setAge(checkList.getAge());
+		currentUserCheckList.setHeight(checkList.getHeight());
+		currentUserCheckList.setIsalcohol(checkList.getIsalcohol());
+		currentUserCheckList.setIssmoke(checkList.getIssmoke());
+		currentUserCheckList.setWeight(checkList.getWeight());
+		currentUserCheckList.setSex(checkList.getSex());
+
 		weightService.addWeight(weight);
-		
+
 		userCheckListService.updateCheckList(currentUserCheckList);
-		
+
 		return new ResponseEntity<UserCheckList>(currentUserCheckList, HttpStatus.OK);
-		
+
 	}
-	
 
 }
