56f2f8e503ea07e2124426881876a4721efc50c7
GyuminDev <pleasure082@gmail.com>
2017-05-18 00:38:57 +0900

    checklist/set 요청 시 몸무게를 Weight테이블에 저장 하는 기능 추가

--- src/main/java/com/mydoctor/controller/UserCheckListController.java
+++ src/main/java/com/mydoctor/controller/UserCheckListController.java
@@ -4,18 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.mydoctor.exception.UserNotFoundException;
-import com.mydoctor.model.UserCheckList;
-import com.mydoctor.model.Weight;
-import com.mydoctor.service.UserCheckListService;
 import com.mydoctor.service.WeightService;
 
 @Controller
