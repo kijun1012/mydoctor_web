a9f5e7b6142cc30fab27d9107fc3c423335edd90
GyuminDev <pleasure082@gmail.com>
2017-05-18 00:28:47 +0900

    addWeight() -> addWeightByList() 변경

--- src/main/java/com/mydoctor/controller/UserCheckListController.java
+++ src/main/java/com/mydoctor/controller/UserCheckListController.java
@@ -1,5 +1,8 @@
 package com.mydoctor.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mydoctor.exception.UserNotFoundException;
 import com.mydoctor.model.UserCheckList;
+import com.mydoctor.model.Weight;
 import com.mydoctor.service.UserCheckListService;
+import com.mydoctor.service.WeightService;
 
 @Controller
 @RequestMapping("/checklist")
@@ -19,6 +24,9 @@ public class UserCheckListController {
 	
 	@Autowired
 	private UserCheckListService userCheckListService;
+	
+	@Autowired
+	private WeightService weightService;
 	
 	@ResponseBody
 	@RequestMapping(value = "/set", method = RequestMethod.POST, produces = "application/json")
@@ -38,6 +46,17 @@ public class UserCheckListController {
 		currentUserCheckList.setIssmoke(checkList.getIssmoke());
 		currentUserCheckList.setWeight(checkList.getWeight());
 		currentUserCheckList.setSex(checkList.getSex());
+		
+		
+		Weight weight = new Weight();
+		weight.setUsername(checkList.getUsername());
+		Date date = new Date();
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String newdate = format1.format(date);
+		weight.setMeasurement_time(newdate);
+		weight.setWeightValue(checkList.getWeight());
+		
+		weightService.addWeight(weight);
 		
 		userCheckListService.updateCheckList(currentUserCheckList);
 		
