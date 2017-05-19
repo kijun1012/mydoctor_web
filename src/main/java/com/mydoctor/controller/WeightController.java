18ca82a36c9b6270aceb75196b336a93f7510f84
kijun <kijun1012@nate.com>
2017-05-19 17:07:25 +0900

    체중 추가할 때 userchecklist에 있는 체중 수정

--- src/main/java/com/mydoctor/controller/WeightController.java
+++ src/main/java/com/mydoctor/controller/WeightController.java
@@ -6,18 +6,25 @@ import java.util.StringTokenizer;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mobile.device.Device;
 import org.springframework.mobile.device.DeviceUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mydoctor.model.HeartRate;
+import com.mydoctor.model.UserCheckList;
 import com.mydoctor.model.Weight;
+import com.mydoctor.service.UserCheckListService;
 import com.mydoctor.service.WeightService;
 
 @Controller
@@ -25,6 +32,9 @@ public class WeightController {
 
 	@Autowired
 	private WeightService weightService;
+	
+	@Autowired
+	private UserCheckListService userCheckListService;
 
 	@RequestMapping("/weight")
 	public String weight(Model model, HttpServletRequest request,
@@ -92,10 +102,21 @@ public class WeightController {
 
 	}
 	
+	@ResponseBody
+	@RequestMapping(value = "/weight/add", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Weight> addWeight(@RequestBody Weight weight) {
+
+		this.weightService.addWeight(weight);
+
+		return new ResponseEntity<Weight>(weight, HttpStatus.OK);
+	}
+	
 	@RequestMapping("/weight/delete/{username}/{measurement_time}")
 	public String deleteProduct(@PathVariable String username,@PathVariable String measurement_time,HttpServletRequest request,RedirectAttributes redirectAttributes){
 		System.out.println(measurement_time);
+		
 		this.weightService.deleteWeight(username,measurement_time);
+	
 		
 		redirectAttributes.addAttribute("username",username);
 		
--- src/main/java/com/mydoctor/service/WeightService.java
+++ src/main/java/com/mydoctor/service/WeightService.java
@@ -1,12 +1,14 @@
 package com.mydoctor.service;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mydoctor.dao.UserCheckListDao;
 import com.mydoctor.dao.WeightDao;
-import com.mydoctor.model.HeartRate;
+import com.mydoctor.model.UserCheckList;
 import com.mydoctor.model.Weight;
 
 @Service
@@ -14,6 +16,8 @@ public class WeightService {
 
 	@Autowired
 	private WeightDao weightDao;
+	@Autowired
+	private UserCheckListDao userCheckListDao;
 
 	public List<Weight> getAllWeight(String userId) {
 		return this.weightDao.getAllWeight(userId);
@@ -27,15 +31,34 @@ public class WeightService {
 		else
 			return weightList.get(weightList.size() - 1);
 	}
-	
-	public List<String> addWeightByList(List<Weight> weightList, String userId){
+
+	public List<String> addWeightByList(List<Weight> weightList, String userId) {
+		
+
 		for (int i = 0; i < weightList.size(); i++) {
 			weightList.get(i).setUsername(userId);
+			double weightValue = weightList.get(i).getWeightValue();
+
+			String pattern = "##.##";
+			DecimalFormat dformat = new DecimalFormat(pattern);
+
+			weightValue = Double.parseDouble(dformat.format(weightValue));
+			weightList.get(i).setWeightValue(weightValue);
+
 		}
-		return this.weightDao.addWeightByList(weightList,userId);
+		
+		
+
+		return this.weightDao.addWeightByList(weightList, userId);
 	}
-	
+
 	public void addWeight(Weight weight) {
+
+		// 추가 할 때 유저 체크리스트 불러서 몸무게 업데이트
+		UserCheckList userCheckList = this.userCheckListDao.findById(weight.getUsername());
+		userCheckList.setWeight(weight.getWeightValue());
+		this.userCheckListDao.updateCheckList(userCheckList);
+
 		this.weightDao.addWeight(weight);
 	}
 
@@ -49,7 +72,14 @@ public class WeightService {
 
 	public void deleteWeight(String username, String measurement_time) {
 		// TODO Auto-generated method stub
-		this.weightDao.deleteWeight(username,measurement_time);
+		this.weightDao.deleteWeight(username, measurement_time);
+
+		// userCheckList에 최근 몸무게 다시 적용
+		Weight weight = this.getRecentWeight(username);
+		UserCheckList userCheckList = this.userCheckListDao.findById(username);
+		userCheckList.setWeight(weight.getWeightValue());
+		this.userCheckListDao.updateCheckList(userCheckList);
+		
 	}
 
 }
