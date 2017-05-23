5d079329070d300397983d731684e9c982aeadb5
GyuminDev <pleasure082@gmail.com>
2017-05-24 01:23:52 +0900

    Model AnalysisData 추가

---
+++ src/main/java/com/mydoctor/model/AnalysisData.java
@@ -0,0 +1,52 @@
+package com.mydoctor.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "analysis_data")
+public class AnalysisData {
+	
+	@Id
+	private String username;
+	
+	@Column(name="BTH_G")
+	private String bth_g;
+	
+	@Column(name = "SBP")
+	private String sbp;
+	
+	@Column(name = "DBP")
+	private String dbp;
+	
+	@Column(name = "FBS")
+	private String fbs;
+	
+	@Column(name = "SEX")
+	private int sex;
+	
+	@Column(name = "BMI")
+	private double bmi;
+	
+	@Column(name = "Class")
+	private String dis;
+	
+	@MapsId("username")
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "username")
+	private User user;
+
+}
