e866f699b2ee6ab5ad29e1271d5db816aed22f16
kijun <kijun1012@nate.com>
2017-06-01 16:52:45 +0900

    체크리스트 수정 추가

--- src/main/java/com/mydoctor/controller/UserCheckListController.java
+++ src/main/java/com/mydoctor/controller/UserCheckListController.java
@@ -1,7 +1,10 @@
 package com.mydoctor.controller;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,21 +38,48 @@ public class UserCheckListController {
 		System.out.println(checkList);
 
 		UserCheckList currentUserCheckList = userCheckListService.findById(checkList.getUsername());
+
 		if (currentUserCheckList == null) {
 			throw new UserNotFoundException(checkList.getUsername());
 		}
 
-		currentUserCheckList.setAge(checkList.getAge());
-		currentUserCheckList.setHeight(checkList.getHeight());
-		currentUserCheckList.setIsalcohol(checkList.getIsalcohol());
-		currentUserCheckList.setIssmoke(checkList.getIssmoke());
-		currentUserCheckList.setWeight(checkList.getWeight());
-		currentUserCheckList.setSex(checkList.getSex());
+		else {
+			// currentUserCheckList.setAge(checkList.getAge());
+			// currentUserCheckList.setHeight(checkList.getHeight());
+			// currentUserCheckList.setIsalcohol(checkList.getIsalcohol());
+			// currentUserCheckList.setIssmoke(checkList.getIssmoke());
+			// currentUserCheckList.setWeight(checkList.getWeight());
+			// currentUserCheckList.setSex(checkList.getSex());
 
-		
-		userCheckListService.updateCheckList(currentUserCheckList);
-		
-		
+			currentUserCheckList = checkList;
+			try {
+				currentUserCheckList.setAge(checkAge(checkList.getBirth()));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			userCheckListService.updateCheckList(currentUserCheckList);
+
+			addWeight(checkList);
+			// // checklist/set 요청 올때 weight을 테이블에 추가
+			// Weight weight = new Weight();
+			// weight.setUsername(checkList.getUsername());
+			// Date date = new Date();
+			// SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd
+			// HH:mm:ss");
+			// String newdate = format1.format(date);
+			// weight.setMeasurement_time(newdate);
+			// weight.setWeightValue(checkList.getWeight());
+			//
+			// weightService.addWeight(weight);
+
+		}
+		return new ResponseEntity<UserCheckList>(currentUserCheckList, HttpStatus.OK);
+
+	}
+
+	private void addWeight(UserCheckList checkList) {
 		// checklist/set 요청 올때 weight을 테이블에 추가
 		Weight weight = new Weight();
 		weight.setUsername(checkList.getUsername());
@@ -60,10 +90,22 @@ public class UserCheckListController {
 		weight.setWeightValue(checkList.getWeight());
 
 		weightService.addWeight(weight);
+	}
 
-	
+	private int checkAge(String birth) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date birthDate = format.parse(birth);
 
-		return new ResponseEntity<UserCheckList>(currentUserCheckList, HttpStatus.OK);
+		GregorianCalendar today = new GregorianCalendar();
+		GregorianCalendar birthDay = new GregorianCalendar();
+		birthDay.setTime(birthDate);
+
+		int age = 0;
+
+		if (today.get(Calendar.DAY_OF_YEAR) < birthDay.get(Calendar.DAY_OF_YEAR)) {
+			age = -1;
+		}
+		return today.get(Calendar.YEAR) - birthDay.get(Calendar.YEAR) + age;
 
 	}
 
